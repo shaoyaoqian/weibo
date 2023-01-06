839465ee5b2ce2a911f0c8c5d764351c285d69fa
Ma Pengfei <115222128+shaoyaoqian@users.noreply.github.com>
2023-01-07 02:36:07 +0800

    [fix]

--- weibospider/spiders/tweet.py
+++ weibospider/spiders/tweet.py
@@ -29,7 +29,27 @@ class TweetSpider(Spider):
             url = f"https://weibo.com/ajax/statuses/mymblog?uid={user_id}&page=1"
             print(url)
             yield Request(url, callback=self.parse, meta={'user_id': user_id, 'page_num': 1})
+    
+    def parse_long_tweet(self, response):
+        """
+        解析长推文
+        """
+        data = json.loads(response.text)['data']
+        item = response.meta['item']
+        item['content'] = data['longTextContent']
+        if 'retweeted' in item and item['retweeted']['isLongText']:
+            url = "https://weibo.com/ajax/statuses/longtext?id=" + item['retweeted']['mblogid']
+            yield Request(url, callback=parse_long_retweeted, meta={'item': item})
+        else:
+            yield item
 
+    def parse_long_retweeted(self, response):
+        data = json.loads(response.text)['data']
+        item = response.meta['item']
+        item['retweeted']['content'] = data['longTextContent']
+        yield item
+
+    
     def parse(self, response, **kwargs):
         """
         网页解析
@@ -48,25 +68,6 @@ class TweetSpider(Spider):
                 yield Request(url, callback=parse_long_retweeted, meta={'item': item})
             else:
                 yield item
-
-        def parse_long_tweet(self, response):
-            """
-            解析长推文
-            """
-            data = json.loads(response.text)['data']
-            item = response.meta['item']
-            item['content'] = data['longTextContent']
-            if 'retweeted' in item and item['retweeted']['isLongText']:
-                url = "https://weibo.com/ajax/statuses/longtext?id=" + item['retweeted']['mblogid']
-                yield Request(url, callback=parse_long_retweeted, meta={'item': item})
-            else:
-                yield item
-
-        def parse_long_retweeted(self, response):
-            data = json.loads(response.text)['data']
-            item = response.meta['item']
-            item['retweeted']['content'] = data['longTextContent']
-            yield item
 
         if tweets:
             user_id, page_num = response.meta['user_id'], response.meta['page_num']
